313ac3dacd19da902243ab3842a285d140e71acd
Pennny <mikailibrahim514@gmail.com>
2025-11-09 20:45:52 +0100

    Prepare for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
+
 export default function App() {
+ 
    const [isMenuOpen, setIsMenuOpen] = useState(false);
    const [formData, setFormData] = useState({
     name: '',
